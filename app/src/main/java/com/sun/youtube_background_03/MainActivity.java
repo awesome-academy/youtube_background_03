fcde7735cd5f33b0a40c97f6337af50bbbf6c0d1
duongngocthach <duongngocthach1997@gmail.com>
2019-04-09 14:18:32 +0700

    main screen

--- app/src/main/java/com/sun/youtube_background_03/MainActivity.java
+++ app/src/main/java/com/sun/youtube_background_03/MainActivity.java
@@ -2,6 +2,9 @@ package com.sun.youtube_background_03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.actionSearch) {
+            return true;
+        }
+        if (item.getItemId() == R.id.actionSetting){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
